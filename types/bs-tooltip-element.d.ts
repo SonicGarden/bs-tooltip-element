f412c58c11eeb9ffebc2e87f2b64e670ac032478
aki <lala.akira@gmail.com>
2023-11-20 12:45:41 +0900

    build

---
+++ types/bs-tooltip-element.d.ts
@@ -0,0 +1,32 @@
+import Tooltip from 'bootstrap/js/dist/tooltip';
+declare const Placements: readonly ["auto", "top", "bottom", "left", "right"];
+type Placement = (typeof Placements)[number];
+export declare class BsTooltipElement extends HTMLElement {
+    static get observedAttributes(): string[];
+    connectedCallback(): void;
+    disconnectedCallback(): void;
+    attributeChangedCallback(name: string): void;
+    private initTooltip;
+    private refresh;
+    private isShown;
+    private showTooltip;
+    private hideTooltip;
+    private update;
+    get manual(): boolean;
+    set manual(value: boolean);
+    get show(): boolean;
+    set show(value: boolean);
+    get disabled(): boolean;
+    set disabled(value: boolean);
+    get tooltip(): Tooltip | undefined;
+    get placement(): Placement;
+    get contentElement(): HTMLElement;
+    get content(): string | undefined;
+    set content(value: string | undefined);
+}
+declare global {
+    interface Window {
+        BsTooltipElement: typeof BsTooltipElement;
+    }
+}
+export {};
